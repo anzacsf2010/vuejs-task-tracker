3e65bf5641c1834834a3e9d44605799d2223e722
anzacsf2010 <anzacosf2010@gmail.com>
2021-12-02 22:23:08 -0400

    Updating files

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+    <p>Copyright &copy; {{ year }}</p>
+    <router-link to="/about">About</router-link>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  props: {
+    year: Number,
+  }
+}
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <h3>Version {{ version }}</h3>
+  <router-link to="/">Go Back</router-link>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+      version: '1.0.0'
+    }
+  },
+  inheritAttrs: false, // disable 'non-props' warning
+};
+</script>
